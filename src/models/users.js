66d81e2ef6aef0fafc74f65d9e6d0b984b3e15a5
Bestie <lucientown@outlook.com>
2017-04-14 21:46:02 +0800

    完成用户注册登陆

---
+++ src/models/users.js
@@ -0,0 +1,102 @@
+import mongoose from 'mongoose'
+import bcrypt from 'bcrypt'
+import config from '../../config'
+import jwt from 'jsonwebtoken'
+import joi from 'joi'
+
+const User = new mongoose.Schema({
+  status: {
+    type: Number,
+    default: 0 //0未激活,1正常,2禁止使用
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  nickname: {
+    type: String,
+    required: true
+  }
+})
+
+User.pre('save', function preSave(next) {
+  const user = this
+
+  user.joiValidate(user,err=>{ //使用joi验证所有参数是否符合要求
+    if (err) {
+      return next(err)
+    } 
+  })
+
+  if (!user.isModified('password')) {
+    return next() //如果未修改密码，则直接修改，不再重新生成密码
+  }
+
+  new Promise((resolve, reject) => {
+      bcrypt.genSalt(10, (err, salt) => {
+        if (err) {
+          return reject(err)
+        }
+        resolve(salt)
+      })
+    })
+    .then(salt => { //将密码加盐hash之后存储，提高安全性
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        if (err) {
+          throw new Error(err)
+        }
+
+        user.password = hash
+
+        next(null)
+      })
+    })
+    .catch(err => next(err))
+})
+
+User.methods.joiValidate = function joiValidate(obj,cb) { //使用joi验证
+  const schema = {
+    status: joi.number(),
+    username: joi.string().token().lowercase().min(4).max(20).required(),
+    email: joi.string().email().lowercase().required(),
+    password: joi.string().alphanum().min(8).max(20),
+    nickname: joi.string().min(4).max(20)
+  }
+  return joi.validate(obj,schema,{allowUnknown:true},cb)
+}
+
+User.methods.validatePassword = function validatePassword(password) {
+  const user = this
+
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(password, user.password, (err, isMatch) => {
+      if (err) {
+        return reject(err)
+      }
+      console.log('isMatch'+password+' '+user.password)
+      resolve(isMatch)
+    })
+  })
+}
+
+User.methods.generateToken = function generateToken() {
+  const user = this
+
+  return jwt.sign({
+    id: user.id
+  }, config.token, {
+    expiresIn: '7 days'
+  })
+}
+
+export default mongoose.model('user', User)
---
+++ src/modules/users/controller.js
@@ -0,0 +1,232 @@
+import User from '../../models/users'
+
+/**
+ * @api {post} /users Create a new user
+ * @apiPermission 所有人
+ * @apiVersion 1.0.0
+ * @apiName CreateUser
+ * @apiGroup Users
+ *
+ * @apiExample 用法示例:
+ * curl -H "Content-Type: application/json" -X POST -d '{ "user": { "username": "johndoe","email": "abc@abc.com", "password": "secretpasas","nickname": "nickname" } }' localhost:5000/users
+ *
+ * @apiParam {Object} user          User 对象 (必须)
+ * @apiParam {String} user.username 用户名,同时是个性链接地址 (必须)
+ * @apiParam {String} user.email 电子邮箱地址
+ * @apiParam {String} user.password 密码
+ *
+ * @apiSuccess {Object}   user           User 对象
+ * @apiSuccess {ObjectId} user._id       User Objectid
+ * @apiSuccess {String}   user.username  用户名,同时是个性链接地址
+ * @apiSuccess {String}   user.email     电子邮箱地址
+ * @apiSuccess {String}   user.nickname  用户昵称
+ *
+ * @apiSuccessExample {json} 成功响应
+ *     HTTP/1.1 200 OK
+ *     {
+ *      "user": {
+ *        "__v": 0,
+ *        "username": "bestie",
+ *        "email": "abc@abc.com",
+ *        "nickname": "BESTIE",
+ *        "_id": "58f09885514d793bb04b0685",
+ *        "status": 0
+ *      },
+ *      "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjU4ZjA5ODg1NTE0ZDc5M2JiMDRiMDY4NSIsImlhdCI6MTQ5MjE2MjY5MywiZXhwIjoxNDkyNzY3NDkzfQ.MDesD1lTHuLYvwOmjgNDpUHr7VqBl6RAWTG-HRRJnXE"
+ *     }
+ *
+ * @apiError UnprocessableEntity 缺少必要的参数或者参数不符合要求
+ *
+ * @apiErrorExample {json} 错误响应
+ *     HTTP/1.1 422 Unprocessable Entity
+ *     {
+ *       "status": 422,
+ *       "error": "Unprocessable Entity"
+ *     }
+ */
+export async function createUser(ctx) {
+  const user = new User(ctx.request.body.user)
+  try {
+    await user.save()
+  } catch (err) {
+    ctx.throw(422, err.message)
+  }
+
+  const token = user.generateToken()
+  const response = user.toJSON()
+
+  delete response.password
+
+  ctx.body = {
+    user: response,
+    token
+  }
+}
+
+/**
+ * @api {get} /users Get all users
+ * @apiPermission user
+ * @apiVersion 1.0.0
+ * @apiName GetUsers
+ * @apiGroup Users
+ *
+ * @apiExample Example usage:
+ * curl -H "Content-Type: application/json" -X GET localhost:5000/users
+ *
+ * @apiSuccess {Object[]} users           Array of user objects
+ * @apiSuccess {ObjectId} users._id       User id
+ * @apiSuccess {String}   users.name      User name
+ * @apiSuccess {String}   users.username  User username
+ *
+ * @apiSuccessExample {json} Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "users": [{
+ *          "_id": "56bd1da600a526986cf65c80"
+ *          "name": "John Doe"
+ *          "username": "johndoe"
+ *       }]
+ *     }
+ *
+ * @apiUse TokenError
+ */
+export async function getUsers(ctx) {
+  const users = await User.find({}, '-password')
+  ctx.body = {
+    users
+  }
+}
+
+/**
+ * @api {get} /users/:id Get user by id
+ * @apiPermission user
+ * @apiVersion 1.0.0
+ * @apiName GetUser
+ * @apiGroup Users
+ *
+ * @apiExample Example usage:
+ * curl -H "Content-Type: application/json" -X GET localhost:5000/users/56bd1da600a526986cf65c80
+ *
+ * @apiSuccess {Object}   users           User object
+ * @apiSuccess {ObjectId} users._id       User id
+ * @apiSuccess {String}   users.name      User name
+ * @apiSuccess {String}   users.username  User username
+ *
+ * @apiSuccessExample {json} Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "user": {
+ *          "_id": "56bd1da600a526986cf65c80"
+ *          "name": "John Doe"
+ *          "username": "johndoe"
+ *       }
+ *     }
+ *
+ * @apiUse TokenError
+ */
+export async function getUser(ctx, next) {
+  try {
+    const user = await User.findById(ctx.params.id, '-password')
+    if (!user) {
+      ctx.throw(404)
+    }
+
+    ctx.body = {
+      user
+    }
+  } catch (err) {
+    if (err === 404 || err.name === 'CastError') {
+      ctx.throw(404)
+    }
+
+    ctx.throw(500)
+  }
+
+  if (next) {
+    return next()
+  }
+}
+
+/**
+ * @api {put} /users/:id Update a user
+ * @apiPermission
+ * @apiVersion 1.0.0
+ * @apiName UpdateUser
+ * @apiGroup Users
+ *
+ * @apiExample Example usage:
+ * curl -H "Content-Type: application/json" -X PUT -d '{ "user": { "name": "Cool new Name" } }' localhost:5000/users/56bd1da600a526986cf65c80
+ *
+ * @apiParam {Object} user          User object (required)
+ * @apiParam {String} user.name     Name.
+ * @apiParam {String} user.username Username.
+ *
+ * @apiSuccess {Object}   users           User object
+ * @apiSuccess {ObjectId} users._id       User id
+ * @apiSuccess {String}   users.name      Updated name
+ * @apiSuccess {String}   users.username  Updated username
+ *
+ * @apiSuccessExample {json} Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "user": {
+ *          "_id": "56bd1da600a526986cf65c80"
+ *          "name": "Cool new name"
+ *          "username": "johndoe"
+ *       }
+ *     }
+ *
+ * @apiError UnprocessableEntity Missing required parameters
+ *
+ * @apiErrorExample {json} Error-Response:
+ *     HTTP/1.1 422 Unprocessable Entity
+ *     {
+ *       "status": 422,
+ *       "error": "Unprocessable Entity"
+ *     }
+ *
+ * @apiUse TokenError
+ */
+export async function updateUser(ctx) {
+  const user = ctx.body.user
+
+  Object.assign(user, ctx.request.body.user)
+
+  await user.save()
+
+  ctx.body = {
+    user
+  }
+}
+
+/**
+ * @api {delete} /users/:id Delete a user
+ * @apiPermission
+ * @apiVersion 1.0.0
+ * @apiName DeleteUser
+ * @apiGroup Users
+ *
+ * @apiExample Example usage:
+ * curl -H "Content-Type: application/json" -X DELETE localhost:5000/users/56bd1da600a526986cf65c80
+ *
+ * @apiSuccess {StatusCode} 200
+ *
+ * @apiSuccessExample {json} Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "success": true
+ *     }
+ *
+ * @apiUse TokenError
+ */
+
+export async function deleteUser(ctx) {
+  const user = ctx.body.user
+
+  await user.remove()
+
+  ctx.status = 200
+  ctx.body = {
+    success: true
+  }
+}
